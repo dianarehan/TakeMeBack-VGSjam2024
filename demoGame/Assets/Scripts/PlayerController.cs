e7f774c89bed2d7fb022b5d5fbce6efd913b3db5
diana <daiana.rehan.dr@gmail.com>
2024-05-03 00:17:27 +0300

    adding some controller logic

--- demoGame/Assets/Scripts/PlayerController.cs
+++ demoGame/Assets/Scripts/PlayerController.cs
@@ -9,70 +9,38 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
     float speed = 5f;
-    bool youCanPickItem = false;
-    [SerializeField] GameObject itemDisplayer;
-    GameObject itemToBeDisplayed;
-    GameObject newItem;
-    private Sprite pickedItemSprite;
+    float verticalSpeed = 3f;
+    bool isFacingRight =true;
+    
 
     void Start()
     {
-        itemDisplayer.GetComponent<Image>();
+       
     }
 
-    // Update is called once per frame
     void Update()
     {
         float horizontalSignal = Input.GetAxis("Horizontal");
         this.transform.Translate(new Vector2(horizontalSignal*Time.deltaTime*speed, 0));
+        if (!isFacingRight && horizontalSignal > 0)
+            Flip();
+        else if (isFacingRight && horizontalSignal < 0)
+            Flip();
+
         float verticalSignal = Input.GetAxis("Vertical");
         this.transform.Translate(new Vector2(0, verticalSignal*Time.deltaTime*speed));
 
 
-        //here i am in range of the item so i have the option to press E and pick it up
-        if (youCanPickItem)
-        {   //here if the player presses E the item is picked then it gets shown on the canvas
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                newItem = Instantiate(itemToBeDisplayed);
-                newItem.SetActive(false); // Deactivate the copied item
-
-                // i save the item's sprite in a variable before destroying it to use it later in putting it on the canvas
-                pickedItemSprite = itemToBeDisplayed.GetComponent<SpriteRenderer>().sprite;
-
-                itemDisplayer.GetComponent<Image>().sprite = pickedItemSprite; 
-                //i picked the item so i destroy it
-                Destroy(itemToBeDisplayed.gameObject);
-    
-             }
-            
-        }
+       
         
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {   
-        if (other.gameObject.CompareTag("Item") )
-        {   
-            itemToBeDisplayed = other.gameObject;
-            
-            other.gameObject.transform.GetChild(0).gameObject.SetActive(true);
-
-            youCanPickItem=true;
-
-            
-
-        }
-        
-    }
-    private void OnTriggerExit2D(Collider2D other)
+    void Flip()
     {
-        if (other.gameObject.CompareTag("Item"))
-        {
-            other.gameObject.transform.GetChild(0).gameObject.SetActive(false);
-            youCanPickItem=false; //beacuse i am not in range of the item, I cant pick it up
-        }
+        isFacingRight = !isFacingRight;
+        Vector3 myScale = this.transform.localScale;
+        myScale.x *= -1;
+        this.transform.localScale=myScale;
     }
-
 
 }
