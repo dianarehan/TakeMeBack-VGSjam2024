41777c13a734bc1f47983d60186779e2681f561c
diana <daiana.rehan.dr@gmail.com>
2024-05-02 14:18:50 +0300

    script added

--- demoGame/Assets/Scripts/PlayerController.cs
+++ demoGame/Assets/Scripts/PlayerController.cs
@@ -1,10 +1,11 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float speed = 5f;
     void Start()
     {
         
@@ -13,6 +14,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalSignal = Input.GetAxis("Horizontal");
+        this.transform.Translate(new Vector2(horizontalSignal*Time.deltaTime*speed, 0));
+        float verticalSignal = Input.GetAxis("Vertical");
+        this.transform.Translate(new Vector2(0, verticalSignal*Time.deltaTime*speed));
     }
 }
