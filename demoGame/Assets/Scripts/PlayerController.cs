66140f920b7475fe28701a7cadad7fe453486443
0rahma0 <03rahma.ahmed@gmail.com>
2024-05-04 01:33:47 +0300

    lvl 1 modified

--- demoGame/Assets/Scripts/PlayerController.cs
+++ demoGame/Assets/Scripts/PlayerController.cs
@@ -13,7 +13,7 @@ public class PlayerController : MonoBehaviour
     float verticalSpeed = 9f;
     float jumpSpeed = 5f;
 
-    bool isFacingRight =true;
+    bool isFacingRight = true;
     bool isFacingALadder = false;
     bool isGrounded = true;
     bool donePrinting = false;
@@ -26,6 +26,9 @@ public class PlayerController : MonoBehaviour
 
     public GameObject camera;
     LevelManager manager;
+    AudioSource audioSource;
+    public AudioClip portalSound;
+    private GameObject portal2Object;
 
     MessageDisplay messageDisplay;
 
@@ -33,36 +36,38 @@ public class PlayerController : MonoBehaviour
     {
         LoadAccumulatedScore();
         rb = this.GetComponent<Rigidbody2D>();
-        manager =camera.GetComponent<LevelManager>();
-        messageDisplay= camera.GetComponent<MessageDisplay>();
+        manager = camera.GetComponent<LevelManager>();
+        messageDisplay = camera.GetComponent<MessageDisplay>();
+        audioSource = camera.GetComponent<AudioSource>();
+        portal2Object = GameObject.Find("SecondPortal");
         //gameObject.transform.position= checkPoints[0].transform.position;
         Debug.Log(fragmentScore);
         Debug.Log("Accumulated Score: " + PlayerPrefs.GetInt("AccumulatedScore", 0));
-
+        Debug.Log(portal2Object.name);
     }
 
     void Update()
     {
         float horizontalSignal = Input.GetAxis("Horizontal");
-        this.transform.Translate(new Vector2(horizontalSignal*Time.deltaTime*speed, 0));
+        this.transform.Translate(new Vector2(horizontalSignal * Time.deltaTime * speed, 0));
         if (!isFacingRight && horizontalSignal > 0)
             Flip();
         else if (isFacingRight && horizontalSignal < 0)
             Flip();
 
         float verticalSignal = Input.GetAxis("Vertical");
-            if(isFacingALadder)
-                this.transform.Translate(new Vector2(0, verticalSignal*Time.deltaTime* verticalSpeed));
+        if (isFacingALadder)
+            this.transform.Translate(new Vector2(0, verticalSignal * Time.deltaTime * verticalSpeed));
 
         float jumpingSignal = Input.GetAxis("Jump");
         if (jumpingSignal > 0 && isGrounded)
             Jump();
-        if (fragmentScore == 6&&!donePrinting)
+        if (fragmentScore == 6 && !donePrinting)
         {
             StartCoroutine(messageDisplay.DisplayMessage());
             donePrinting = true;
         }
-        if(fragmentScore==7)
+        if (fragmentScore == 4)
             CompleteLevel();
     }
 
@@ -71,7 +76,7 @@ public class PlayerController : MonoBehaviour
         isFacingRight = !isFacingRight;
         Vector3 myScale = this.transform.localScale;
         myScale.x *= -1;
-        this.transform.localScale=myScale;
+        this.transform.localScale = myScale;
     }
     void Jump()
     {
@@ -89,7 +94,11 @@ public class PlayerController : MonoBehaviour
             AddScore(1);
             Destroy(collision.gameObject);
         }
-        
+        if (collision.CompareTag("Portal1"))
+        {
+            StartCoroutine(TeleportAfterDelay());
+        }
+
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
@@ -97,6 +106,7 @@ public class PlayerController : MonoBehaviour
         {
             isFacingALadder = false;
         }
+
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -115,7 +125,7 @@ public class PlayerController : MonoBehaviour
     }
     void AddScore(int score)
     {
-        fragmentScore+=score;
+        fragmentScore += score;
         manager.PlaySound();
         manager.UpdateScore(fragmentScore);
     }
@@ -125,8 +135,8 @@ public class PlayerController : MonoBehaviour
     public void SaveLocalScore()
     {
         finalScore += fragmentScore;
-        fragmentScore = 0; 
-        SaveAccumulatedScore(); 
+        fragmentScore = 0;
+        SaveAccumulatedScore();
     }
 
     // Function to save the accumulated score
@@ -145,8 +155,8 @@ public class PlayerController : MonoBehaviour
     public void CompleteLevel()
     {
         SaveLocalScore(); // Save the local score to the accumulated score
-        
-        if(SceneManager.GetActiveScene().name=="level2")
+
+        if (SceneManager.GetActiveScene().name == "level2")
             SceneManager.LoadScene("mainMenu");
         else
         {
@@ -155,5 +165,15 @@ public class PlayerController : MonoBehaviour
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
         }
+    }
+    IEnumerator TeleportAfterDelay()
+    {
+        yield return new WaitForSeconds(0.5f); // Wait for one second
+
+        // Move the object to the position of portal2Object
+        this.gameObject.transform.localPosition = portal2Object.transform.localPosition;
+
+        // Play the portal sound
+        audioSource.PlayOneShot(portalSound);
     }
 }
